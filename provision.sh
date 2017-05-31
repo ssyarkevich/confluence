21551af2d08c24ee9872dbb0ba5d021481954702
Stanislav Syarkevich <ssyarkevich@hotmail.com>
2017-06-01 02:22:47 +0300

    Some bug in vagrant...

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 echo "Starting deploying our app..."
-#yum install -y wget && wget --max-redirect 1 --progress=bar:force https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-6.2.1-x64.bin
-curl --remote-name -# --location https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-6.2.1-x64.bin
+yum install -y wget 
+wget --max-redirect 3 --progress=bar:force https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-6.2.1-x64.bin
+#curl --remote-name -# --location https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-6.2.1-x64.bin
 chmod a+x atlassian-confluence-6.2.1-x64.bin && cp /vagrant/response.varfile .
 sudo ./atlassian-confluence-6.2.1-x64.bin -q varfile response.varfile
