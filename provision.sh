c99211554a4d5842395ce7aec73bdb530f18a874
Stanislav Syarkevich <ssyarkevich@hotmail.com>
2017-06-01 11:07:38 +0300

    SSL

--- provision.sh
+++ provision.sh
@@ -5,12 +5,13 @@ yum install -y nginx && mkdir -p /etc/nginx/ssl/confluence.ssyarkev.com/ && cd /
 openssl req -nodes -newkey rsa:2048 -keyout self-ssl.key -out self-ssl.csr -subj "/C=UA/ST=Lviv region/L=Lviv/O=Epam student/OU=IT Department/CN=confluence.ssyarkev.com"
 openssl x509 -req -days 365 -in self-ssl.csr -signkey self-ssl.key -out self-ssl.crt && chown nginx:nginx self* && chmod 660 self* && chown nginx:nginx self*
 mkdir -p /etc/nginx/sites-available && mkdir -p /etc/nginx/sites-enabled
-cd /vagrant && cat nginx_config > /etc/nginx/sites-available/confluence.ssyarkev.com && cat nginx.conf > /etc/nginx/nginx.conf
+cd /vagrant && cat confluence.ssyarkev.com > /etc/nginx/sites-available/confluence.ssyarkev.com && cat nginx.conf > /etc/nginx/nginx.conf
 chown -R nginx:nginx /etc/nginx/sites-available/confluence.ssyarkev.com
 chown -R nginx:nginx /etc/nginx/
 ln -s /etc/nginx/sites-available/confluence.ssyarkev.com /etc/nginx/sites-enabled/confluence.ssyarkev.com
 #rm /etc/nginx/sites-enabled/default
-systemctl restart nginx
+echo 0 > /sys/fs/selinux/enforce
+#systemctl restart nginx
 echo "================================================================================================"
 echo "Moving on...."
 if [ ! -f ./atlassian-confluence-6.2.1-x64.bin ]; then
@@ -20,3 +21,6 @@ fi
 #curl --remote-name -# --location https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-6.2.1-x64.bin
 chmod a+x atlassian-confluence-6.2.1-x64.bin # && cp /vagrant/response.varfile .
 sudo ./atlassian-confluence-6.2.1-x64.bin -q varfile response.varfile
+cat ./vagrant/server.xml > /opt/Confluence/conf/server.xml
+/etc/init.d/confluence stop && /etc/init.d/confluence start
+systemctl restart nginx
