0d8570bdd66319e4ae2f02ec1b22828aa055c918
Stanislav Syarkevich <ssyarkevich@hotmail.com>
2017-06-06 03:26:35 +0300

    Using docker

--- Vagrantfile
+++ Vagrantfile
@@ -2,9 +2,9 @@
 # vi: set ft=ruby :
 Vagrant.configure("2") do |config|
   config.vm.box = "centos/7"
-  #config.vm.network "public_network", ip: "192.168.255.10"# config.vm.synced_folder "../data", "/vagrant_data"
-  #config.ssh.insert_key = false
-  #config.vm.provision "file", source: "id_rsa.pub", destination: "/home/vagrant/.ssh/authorized_keys"
+  config.vm.provision "docker" do |d|
+  d.pull_images "mysql"
+  end
   config.vm.provision "shell", path: "provision.sh"
   config.vm.network :forwarded_port, guest: 443, host: 8888, host_ip: "127.0.0.1"
   config.vm.network :forwarded_port, guest: 8090, host: 9999, host_ip: "127.0.0.1"
--- provision.sh
+++ provision.sh
@@ -14,22 +14,30 @@ setenforce 0
 systemctl enable nginx
 echo "================================================================================================"
 echo "Moving on...."
-if [ ! -f ./atlassian-confluence-6.2.1-x64.bin ]; then
- yum install -y wget 
- wget --max-redirect 3 --progress=bar:force https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-6.2.1-x64.bin
+yum install -y wget
+echo "Downloading MySql driver ..................."
+wget --progress=bar:force https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.42.tar.gz
+echo "Extracting tar..." && tar -xvzf mysql-connector-java-5.1.42.tar.gz
+if [ ! -f ./atlassian-confluence-6.2.1-x64.bin ]; then 
+  wget --max-redirect 3 --progress=bar:force https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-6.2.1-x64.bin
 fi
 #curl --remote-name -# --location https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-6.2.1-x64.bin
 chmod a+x atlassian-confluence-6.2.1-x64.bin # && cp /vagrant/response.varfile .
 sudo ./atlassian-confluence-6.2.1-x64.bin -q varfile response.varfile
+cp ./mysql-connector-java-5.1.42/mysql-connector-java-5.1.42-bin.jar /opt/Confluence/confluence/WEB-INF/lib && rm -rf mysql*
 cd /vagrant
 cat ./server.xml > /opt/Confluence/conf/server.xml
-echo "----------------------------------------------------------------"
 cp /vagrant/id_rsa /home/vagrant/.ssh/id_rsa
 chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
 chmod 600 /home/vagrant/.ssh/id_rsa 
-ls -lah /home/vagrant/.ssh/
+mkdir /etc/mysql && cp /vagrant/my.cnf /etc/mysql
+echo "*************************************************************************************************"
+docker run --name MYSQL -v /my/custom:/etc/mysql/conf.d -p 3306:3306 -e MYSQL_RANDOM_ROOT_PASSWORD=yes -d mysql
+docker logs MYSQL |grep "GENERATED"
+echo "*************************************************************************************************"
 #rm -f *.bin && rm -f id_rsa
 /etc/init.d/confluence stop 2>>/dev/null && /etc/init.d/confluence start
 systemctl restart nginx
-sudo ssh -i /home/vagrant/.ssh/id_rsa -vvv -o StrictHostKeyChecking=no -f2NR 0.0.0.0:2222:localhost:443 ec2-user@ec2-52-212-113-156.eu-west-1.compute.amazonaws.com
-echo "http://ec2-52-212-113-156.eu-west-1.compute.amazonaws.com:2222/confluence"
+sudo ssh -i /home/vagrant/.ssh/id_rsa -vvv -o StrictHostKeyChecking=no -f2NR 0.0.0.0:2222:localhost:443 ec2-user@ec2-52-215-75-94.eu-west-1.compute.amazonaws.com
+
+echo "ec2-52-215-75-94.eu-west-1.compute.amazonaws.com:2222/confluence"
